1884a95c6fc8a34f85449f30f18229e5b9b0204c
Kent Jordan <kentaki.dev@gmail.com>
2023-05-02 20:52:48 +0800

    Added loading component for /login route and made a new variable for axios.post()'s URL

--- src/app/login/api/index.ts
+++ src/app/login/api/index.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import { ILoginForm, ILoginResponse } from '../types/types';
 
+const SERVER_URL = `http://localhost:3001/login`;
+
 export const login = (data: ILoginForm | unknown) => {
-    return axios.post<ILoginResponse>("http://localhost:3001/login", data)
+    return axios.post<ILoginResponse>(SERVER_URL, data)
 };
