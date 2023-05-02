5cd2567ccb6bad2f412c49998c28a1e8a5491aec
Kent Jordan <kentaki.dev@gmail.com>
2023-05-02 08:50:59 +0800

    Format account/page.tsx

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -10,12 +10,9 @@ import { useSelector } from 'react-redux';
 
 const AccountPage = () => {
   const {
-    user: { email, cart_items, first_name, last_name, orders, gender },
-    isAuthenticated,
-  } = useSelector(
-    (state: IStoreState) => state.UserReducer
-  ) as IUserReducerState;
-
+    user: { email, first_name, last_name, gender },
+  } = useSelector((state: IStoreState) => state.UserReducer) as IUserReducerState;
+  
   return (
     <div className="h-full w-full flex  justify-center items-center bg-stone-50 ">
       <div className="flex">
@@ -23,9 +20,7 @@ const AccountPage = () => {
         <div className="flex flex-col w-[500px] px-8 py-6 rounded shadow bg-white mx-8">
           <div className="flex flex-col">
             <span className="font-bold">My Profile </span>
-            <span className="text-stone-500 text-sm">
-              Manage and protect your account
-            </span>
+            <span className="text-stone-500 text-sm">Manage and protect your account</span>
             <hr className="my-4" />
           </div>
           <div className="flex items-center flex-col my-6">
@@ -55,7 +50,11 @@ const AccountPage = () => {
                 <span className=" text-sm">09123456789</span>
                 <BsPencilSquare className="mx-2 text-cyan-700 cursor-pointer" />
               </div>
-              <form className="my-3 text-sm flex" action="" method="post">
+              <form
+                className="my-3 text-sm flex"
+                action=""
+                method="post"
+              >
                 <div>
                   <input
                     type="radio"
@@ -63,7 +62,10 @@ const AccountPage = () => {
                     id="male"
                     defaultChecked={!!gender}
                   />
-                  <label className="mx-1" htmlFor="male">
+                  <label
+                    className="mx-1"
+                    htmlFor="male"
+                  >
                     Male
                   </label>
                 </div>
@@ -74,16 +76,17 @@ const AccountPage = () => {
                     id="female"
                     defaultChecked={!gender}
                   />
-                  <label className="mx-1" htmlFor="female">
+                  <label
+                    className="mx-1"
+                    htmlFor="female"
+                  >
                     Female
                   </label>
                 </div>
               </form>
             </div>
           </div>
-          <button className="px-4 py-2 mt-8 text-sm rounded text-stone-100 bg-stone-900">
-            Save
-          </button>
+          <button className="px-4 py-2 mt-8 text-sm rounded text-stone-100 bg-stone-900">Save</button>
         </div>
       </div>
     </div>
