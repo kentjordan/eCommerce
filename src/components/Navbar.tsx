7a92d5c29e5c209a060cf05c58ee8ce1a5b0cbc4
Kent Jordan <kentaki.dev@gmail.com>
2023-05-02 15:28:30 +0800

    Added a popup component for Account Menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,41 +1,54 @@
 import Link from 'next/link';
 import { BsPersonCircle, BsSearch, BsCart } from 'react-icons/bs';
+import Image from 'next/image';
+import { MdContentPaste, MdLogout, MdSettings, MdArrowForwardIos } from 'react-icons/md';
+import AccountMenu from './AccountMenu';
+import { IStoreState } from '@/redux/Store';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleAccountMenu } from '@/redux/slices/AccountMenu';
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const { isOpened } = useSelector((state: IStoreState) => state.AccountMenuReducer);
+
   return (
-    <div className="flex justify-between w-full bg-stone-900 text-stone-100 items-center px-2 absolute">
-      {/* <ul className='flex'>
+    <>
+      <div className="flex justify-between w-full bg-stone-900 text-stone-100 items-center px-2  z-50">
+        {/* <ul className='flex'>
         <li className='m-2 text-sm'>Contact</li>
         <li className='m-2 text-sm'>About us</li>
         <li className='m-2 text-sm'>Privacy Policy</li>
       </ul> */}
 
-      <div className="m-2">
-        <Link
-          href="/"
-          className="text-2xl font-bold"
-        >
-          eCommerce
-        </Link>
-        <span className="text-sm mx-2 text-stone-400">by Kent Jordan</span>
-      </div>
+        <div className="m-2">
+          <Link
+            href="/"
+            className="text-2xl font-bold"
+          >
+            eCommerce
+          </Link>
+          <span className="text-sm mx-2 text-stone-400">by Kent Jordan</span>
+        </div>
 
-      <div>
-        <ul className="flex">
-          <li className="m-2">
-            <BsSearch />
-          </li>
-          <li className="m-2">
-            <BsCart />
-          </li>
-          <li className="m-2">
-            <Link href="/account">
-              <BsPersonCircle />
-            </Link>
-          </li>
-        </ul>
+        <div className="">
+          <ul className="flex relative">
+            <li className="m-2">
+              <BsSearch />
+            </li>
+            <li className="m-2">
+              <BsCart />
+            </li>
+            <li className="m-2 relative">
+              <BsPersonCircle
+                className="cursor-pointer"
+                onClick={() => dispatch(toggleAccountMenu())}
+              />
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+      <AccountMenu />
+    </>
   );
 };
 
