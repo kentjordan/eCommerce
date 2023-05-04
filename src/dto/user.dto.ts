a777536d3ff285cc0744e26b73da61f8855ac0a3
Kent Jordan <kentaki.dev@gmail.com>
2023-05-04 19:23:41 +0800

    Updated user.dto.ts

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -2,7 +2,7 @@ export class User {
     constructor(
         public email: string,
         public first_name: string,
-        public last_name: true,
+        public last_name: string,
         public cart_items: unknown[],
         public orders: unknown[],
         public gender: number,
