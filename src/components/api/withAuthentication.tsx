3802c7efa9dce6746e14fd914c362a4a75c05be9
Kent Jordan <kentaki.dev@gmail.com>
2023-05-04 19:21:18 +0800

    Moved the logic of useAuthRedirection.ts hook into withAuthentication.tsx file and deleted it

--- src/components/api/withAuthentication.tsx
+++ src/components/api/withAuthentication.tsx
@@ -1,25 +1,30 @@
 import UnauthorizedPage from '@/components/Unauthorized.page';
 import { IUserReducerState } from '@/app/account/types';
-import useAuthRedirection from '@/hooks/useAuthRedirection';
 import { IStoreState } from '@/redux/Store';
 import { IRootLayoutArgs } from '@/types';
 import { useSelector } from 'react-redux';
 import { IWithAuthentication } from '../types';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 const withAuthentication: IWithAuthentication = (
   RootComponent: FunctionComponent<IRootLayoutArgs>,
   { isRouteSecured }
 ) => {
   return (({ children }) => {
+    const router = useRouter();
+
     const { isAuthenticated } = useSelector((state: IStoreState) => state.UserReducer) as IUserReducerState;
-    
-    useAuthRedirection({
-      isRouteSecured,
-      isAuthenticated,
-      redirectDelay: 1500,
-      redirectTo: '/login',
-    });
+
+    useEffect(() => {
+      if (isRouteSecured && !isAuthenticated) {
+        router.push('/login');
+      } else {
+        if (!isAuthenticated) {
+          router.push('/login');
+        }
+      }
+    }, [isAuthenticated]);
 
     if (isRouteSecured && isAuthenticated) return <RootComponent>{children}</RootComponent>;
 
