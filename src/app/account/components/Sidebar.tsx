582d3fd7ab3c1538b9dde3c022460d1e75caacdc
Kent Jordan <kentaki.dev@gmail.com>
2023-05-05 09:25:46 +0800

    Refactored: the Sidebar, and added menu button for controlling the state of account page menu popup

--- src/app/account/components/Sidebar.tsx
+++ src/app/account/components/Sidebar.tsx
@@ -1,43 +1,68 @@
 import Link from 'next/link';
+import React from 'react';
 import { BsPersonCircle, BsReceipt } from 'react-icons/bs';
-
+import { FiMenu } from 'react-icons/fi';
+import { toggleAccountPageMenu } from '@/redux/slices/PopupMenuController.slice';
+import { useDispatch } from 'react-redux';
 const Sidebar = () => {
+  const dispatch = useDispatch();
+
   return (
-    <div className="flex flex-col">
-      <section className="my-3">
-        <div className="flex items-center text-lg">
-          <BsPersonCircle className="mx-1" />
-          <Link
-            href="/account"
-            className="font-bold mx-1"
-          >
-            Account
-          </Link>
-        </div>
-        <ul className="mx-8">
-          <li className="text-stone-500">
-            <Link href="/account/profile">Profile</Link>
-          </li>
-          <li className="text-stone-500">
-            <Link href="/account/addresses">Addresses</Link>
-          </li>
-          <li className="text-stone-500">
-            <Link href="/account/password">Change Password</Link>
-          </li>
-        </ul>
-      </section>
-      <section className="my-3">
-        <div className="flex items-center text-lg">
-          <BsReceipt className="mx-1" />
-          <Link
-            href="/account/purchase"
-            className="font-bold mx-1"
-          >
-            Purchase
-          </Link>
-        </div>
-      </section>
-    </div>
+    <>
+      <FiMenu
+        onClick={() => {
+          dispatch(toggleAccountPageMenu());
+        }}
+        size={24}
+        className="sm:hidden flex my-8 cursor-pointer"
+      />
+      <div className="sm:flex hidden flex-col">
+        <section className="my-3">
+          <div className="flex items-center text-lg rounded m-2 hover:bg-stone-200 px-2 py-1">
+            <BsPersonCircle className="mx-1" />
+            <Link
+              href="/account"
+              className="font-bold mx-1"
+            >
+              Account
+            </Link>
+          </div>
+          <div className="flex flex-col ">
+            <Link
+              href="/account/profile"
+              className="hover:bg-stone-200 px-2 py-1 rounded sm:mx-10 sm:text-start text-center"
+            >
+              Profile
+            </Link>
+
+            <Link
+              href="/account/addresses"
+              className="hover:bg-stone-200 px-2 py-1 rounded sm:mx-10 sm:text-start text-center"
+            >
+              Addresses
+            </Link>
+
+            <Link
+              href="/account/password"
+              className="hover:bg-stone-200 px-2 py-1 rounded sm:mx-10 sm:text-start text-center"
+            >
+              Change Password
+            </Link>
+          </div>
+        </section>
+        <section className="my-4 flex items-center text-lg rounded m-2 hover:bg-stone-200 px-2 py-1">
+          <div className="flex items-center text-lg">
+            <BsReceipt className="mx-1" />
+            <Link
+              href="/account/purchase"
+              className="font-bold mx-1"
+            >
+              Purchase
+            </Link>
+          </div>
+        </section>
+      </div>
+    </>
   );
 };
 
